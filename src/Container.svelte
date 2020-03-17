f1669c3a71a3c01c7e7ce11715d5795111efe4ce
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-03-17 19:34:30 -0300

    Add top-level components.
    Created App, Container, Chat and Login.

---
+++ src/Container.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Login from './Login.svelte'
+  import Chat from './Chat.svelte'
+
+  let loggedIn = false
+
+  const handleLogin = () => {
+    loggedIn = true
+  }
+</script>
+
+{#if !loggedIn}
+  <Login on:login={handleLogin}/>
+{:else}
+  <Chat/>
+{/if}
---
+++ src/Login.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  let username
+  const handleSubmit = () => dispatch('login', { username })
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input
+    bind:value={username}
+    placeholder="Insert your name"
+    minlength=1
+    maxlength=32
+    required
+  >
+  <button>
+    Submit
+  </button>
+</form>
