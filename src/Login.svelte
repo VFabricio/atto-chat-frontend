bb07a34d4ac9ebc02a04ac2e6b0f3c434c329e8c
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2020-03-22 18:34:30 -0300

    Style Login component

--- src/Login.svelte
+++ src/Login.svelte
@@ -7,15 +7,27 @@
   const handleSubmit = () => dispatch('login', { username })
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-  <input
-    bind:value={username}
-    placeholder="Insert your name"
-    minlength=1
-    maxlength=32
-    required
-  >
-  <button>
-    Submit
-  </button>
-</form>
+<style>
+  main {
+    background-color: var(--color-primary-light);
+    margin: 3em 0;
+    padding: var(--spacing);
+  }
+</style>
+
+<main>
+  <p> To begin, insert your name:</p>
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <input
+      bind:value={username}
+      placeholder="Your name here"
+      minlength=1
+      maxlength=32
+      required
+    >
+    <button>
+      Submit
+    </button>
+  </form>
+</main>
